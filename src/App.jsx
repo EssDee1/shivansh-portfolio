b7c8b6615eb82a82b3f9da2e9f74f898d26419e9
Shivansh Dutta <109021679+EssDee1@users.noreply.github.com>
2023-08-26 00:44:53 +0545

    changed pc image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 const App = () => {
 
